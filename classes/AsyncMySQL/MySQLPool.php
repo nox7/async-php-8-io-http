6c2f79b2ba56f4252896f39c40198c46e30d3462
nox7 <17110935+nox7@users.noreply.github.com>
2021-04-06 09:04:47 -0500

    Create MySQLPool.php

---
+++ classes/AsyncMySQL/MySQLPool.php
@@ -0,0 +1,46 @@
+<?php
+	require_once __DIR__ . "/AsyncMySQL.php";
+
+	class MySQLPool{
+
+		protected array $asyncMySQLs = [];
+
+		public function __construct(
+			public int $numConnections,
+			public string $host,
+			public string $user,
+			private string $password,
+			public string $database,
+			public int $port = 3306
+		){
+			for ($i = 0; $i < $numConnections; $i++){
+				$this->asyncMySQLs[] = new AsyncMySQL($host, $user, $password, $database, $port);
+			}
+		}
+
+		/**
+		* Runs an asynchronous ::execute() on an available AsyncMySQL object.
+		* If none are available, will wait until one is.
+		*/
+		public function execute(string $query, array $namedArgs = []): Fiber{
+			$asyncMySQL = null;
+
+			return new Fiber(function() use ($query, $namedArgs){
+				do{
+					foreach ($this->asyncMySQLs as $obj){
+						if ($obj->isAvailable){
+							$asyncMySQL = $obj;
+							break;
+						}
+					}
+
+					if ($asyncMySQL === null){
+						Fiber::suspend();
+					}
+				} while ($asyncMySQL === null);
+
+				return Async::await($asyncMySQL->execute($query, $namedArgs));
+			});
+
+		}
+	}
